eac64592ad94f4c6d9481645e2e92503971861e1
Andrew Miller <206alm@gmail.com>
2019-09-29 13:24:58 -0700

    all topic button clickables now function properly

--- assets/script.js
+++ assets/script.js
@@ -9,10 +9,10 @@ $(document).ready(function() {
     console.log('document ready');
     buttonGenerate(topics);
     addButton();
-    displayGif();
 });
-
-
+$(document).ready(function() {
+    topicButtonClick();
+})
 
 //============================================================================================
 
@@ -32,24 +32,24 @@ function buttonGenerate(topics) {
 //function to add buttons
 function addButton() {
         $('#searchButton').on('click', function(event) {
-            console.log('ive been clicked');
+            //console.log('ive been clicked');
             event.preventDefault();
             var newTerm = $('#searchField').val().trim();
-            console.log(newTerm);
+            //console.log(newTerm);
             topics.push(newTerm);
-            console.log(topics);
+            //console.log(topics);
             buttonGenerate(topics);
         });
 }
 
 //topic button click handler
-function displayGif() {
-    $('#reactButton').on("click", function(event) {
-        console.log('ive been clicked');
+function topicButtonClick() {
+    $(document).on("click", '#reactButton', function(event) {
+        event.preventDefault();
         var searchTerm = $(this).attr('data-name');
+        console.log('ive been clicked');
         console.log(searchTerm);
-        var queryURL =  'https://api.giphy.com/v1/gifs/search?q=' + searchTerm + '&api_key=CGubh0n51S9H6ZsoKjiYx8U4PO3IO17X&limit=10';
-
+        var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + searchTerm + '&api_key=CGubh0n51S9H6ZsoKjiYx8U4PO3IO17X&limit=10';
         $.ajax({
             url: queryURL,
             method: "GET"
